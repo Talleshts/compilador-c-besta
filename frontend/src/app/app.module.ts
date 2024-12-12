ffdbb3c8413ccbaa682826c285a3282038cae3b3
Talles <81382968+Talleshts@users.noreply.github.com>
2024-12-11 22:15:21 -0300

    Ajuste de declaratrions

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'; // Importar HttpClientModule
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 import { ExemploComponent } from './modules/exemplo/exemplo.component';
-import { ExemploStore } from './stores/exemplo.store'; // Importar ExemploStore
 
 @NgModule({
-  declarations: [AppComponent, ExemploComponent],
+  //Colocar o declaration
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule, // Adicionar HttpClientModule
+    AppComponent,
+    ExemploComponent,
   ],
-  providers: [ExemploStore], // Adicionar ExemploStore aos providers
+  providers: [],
 })
 export class AppModule {}
