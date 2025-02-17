a5fbcc6e709bb761c9f336c1941a295ff7566533
Talles <81382968+Talleshts@users.noreply.github.com>
2025-02-16 22:47:55 -0300

    Configure backend for deployment and update project settings

--- frontend/src/app/store/code-store.service.ts
+++ frontend/src/app/store/code-store.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { ErroSintatico } from '../interface/erro-sintatico.interface';
 import { CustomToken } from '../interface/token.interface';
 
@@ -8,7 +9,7 @@ import { CustomToken } from '../interface/token.interface';
   providedIn: 'root',
 })
 export class CodeStoreService {
-  private apiUrl = 'http://localhost:8080/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://compilador-c-besta-production.up.railway.app/api',
+};
